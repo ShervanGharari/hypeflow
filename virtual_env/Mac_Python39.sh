f37afbdc8f7a280fe4a578f69e5c84748c7c042f
ShervanGharari <sh.gharari@gmail.com>
2023-12-22 11:15:43 -0600

    data is added

--- virtual_env/Mac_Python39.sh
+++ virtual_env/Mac_Python39.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-rm -rf fhimp_venv
+rm -rf hypeflow_venv
 
 python3.9 -m pip install virtualenv
-python3.9 -m virtualenv fhimp_venv
-source fhimp_venv/bin/activate
+python3.9 -m virtualenv hypeflow_venv
+source hypeflow_venv/bin/activate
 
 python3.9 -m pip install --upgrade pip
 
@@ -14,15 +14,8 @@ python3.9 -m pip install geopandas
 python3.9 -m pip install networkx
 python3.9 -m pip install jupyter
 python3.9 -m pip install ipykernel
+python3.9 -m pip install numpy
 
-# assuming that dependecies will work
-# python3.9 -m pip install -r requirements.txt
-
-# packages to be added to requirments or release on pypi
-python3.9 -m pip install git+https://github.com/kasra-keshavarz/hydrant
-python3.9 -m pip install git+https://github.com/kasra-keshavarz/meshflow
-python3.9 -m pip install git+https://github.com/ShervanGharari/EASYMORE.git@develop_2.0.0
-
-# add the virtual env to kernel and list
-python3.9 -m ipykernel install --name=fhimp_venv # Add the new virtual environment to Jupyter
+# add the ual env to kernel and list
+python3.9 -m ipykernel install --name=hypeflow_venv # Add the new virtual environment to Jupyter
 jupyter kernelspec list # list existing Jupyter virtual environments
